6b0f8f3e2efbff4cc85cd4a47de8fc30d6242bcb
DiegoGarV <dgv3105@gmail.com>
2024-07-18 18:16:27 -0600

    Tercer polígono

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use nalgebra_glm::vec3;
 fn main() {
     let mut framebuffer = Framebuffer::new(800, 600);
     let bg_color = Color::from_hex("black", 0x000000);
-    let outline_color_p1 = Color::from_hex("white", 0xFFFFFF);
+    let outline_color = Color::from_hex("white", 0xFFFFFF);
     let fill_color_p1 = Color::from_hex("yellow", 0xFFFF00);
+    let fill_color_p2 = Color::from_hex("blue", 0x0000FF);
+    let fill_color_p3 = Color::from_hex("red", 0xFF0000);
 
     framebuffer.set_background_color(bg_color);
     framebuffer.clear();
@@ -30,11 +32,26 @@ fn main() {
         vec3(205.0, 410.0, 0.0),
         vec3(193.0, 383.0, 0.0),
     ];
+    framebuffer.draw_polygon(&polygon1, &outline_color, &fill_color_p1);
 
-    
+    // Segundo polígono
+    let polygon2 = vec![
+        vec3(321.0, 335.0, 0.0),
+        vec3(288.0, 286.0, 0.0),
+        vec3(339.0, 251.0, 0.0),
+        vec3(374.0, 302.0, 0.0),
+    ];
+    framebuffer.draw_polygon(&polygon2, &outline_color, &fill_color_p2);
 
-    framebuffer.draw_polygon(&polygon1, &outline_color_p1, &fill_color_p1);
+    // Tercer polígono
+    let polygon3 = vec![
+        vec3(377.0, 249.0, 0.0),
+        vec3(411.0, 197.0, 0.0),
+        vec3(436.0, 249.0, 0.0),
+    ];
+    framebuffer.draw_polygon(&polygon3, &outline_color, &fill_color_p3);
 
+    // Crea el bitmap
     framebuffer.render_buffer("out.bmp").expect("Error al renderizar el buffer a BMP");
 
     println!("Framebuffer rendered to out.bmp"); 
