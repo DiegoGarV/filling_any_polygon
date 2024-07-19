d31bac29d13ed1e15bcfb1762feb534027368d52
DiegoGarV <dgv3105@gmail.com>
2024-07-18 19:28:00 -0600

    Merge branch 'Poligon-1' into main

--- target/debug/build/matrixmultiply-679b8b668bfa71b9/build_script_build-679b8b668bfa71b9.d
+++ target/debug/build/matrixmultiply-679b8b668bfa71b9/build_script_build-679b8b668bfa71b9.d
@@ -1,5 +1,5 @@
-C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\ejercicio_practica\target\debug\build\matrixmultiply-679b8b668bfa71b9\build_script_build-679b8b668bfa71b9.exe: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\matrixmultiply-0.3.8\build.rs
+C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\filling_any_polygon\target\debug\build\matrixmultiply-679b8b668bfa71b9\build_script_build-679b8b668bfa71b9.exe: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\matrixmultiply-0.3.8\build.rs
 
-C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\ejercicio_practica\target\debug\build\matrixmultiply-679b8b668bfa71b9\build_script_build-679b8b668bfa71b9.d: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\matrixmultiply-0.3.8\build.rs
+C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\filling_any_polygon\target\debug\build\matrixmultiply-679b8b668bfa71b9\build_script_build-679b8b668bfa71b9.d: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\matrixmultiply-0.3.8\build.rs
 
 C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\matrixmultiply-0.3.8\build.rs:
--- target/debug/deps/num_rational-5dd5864a51aab52c.d
+++ target/debug/deps/num_rational-5dd5864a51aab52c.d
@@ -1,6 +1,6 @@
-C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\ejercicio_practica\target\debug\deps\libnum_rational-5dd5864a51aab52c.rmeta: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\lib.rs C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\pow.rs
+C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\filling_any_polygon\target\debug\deps\libnum_rational-5dd5864a51aab52c.rmeta: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\lib.rs C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\pow.rs
 
-C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\ejercicio_practica\target\debug\deps\num_rational-5dd5864a51aab52c.d: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\lib.rs C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\pow.rs
+C:\Users\dgv31\OneDrive\Documents\Universidad\Semestre 6\Graficas\filling_any_polygon\target\debug\deps\num_rational-5dd5864a51aab52c.d: C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\lib.rs C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\pow.rs
 
 C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\lib.rs:
 C:\Users\dgv31\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-rational-0.4.2\src\pow.rs:
